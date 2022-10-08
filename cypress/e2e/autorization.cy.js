3c1b5c764672ae9fbbbbb96f0ae89437f2baf514
Vladyslav Starynets <globaxv@gmail.com>
2022-10-08 23:00:49 +0200

    add fix

--- cypress/e2e/autorization.cy.js
+++ cypress/e2e/autorization.cy.js
@@ -4,16 +4,13 @@ import user from '../fixtures/example.json'
 
 it('Registration', () => {
   
-  cy.visit('/')
+  cy.visit('/index.php?rt=account/login')
   cy.get('#loginFrm_loginname').type(user.userName)
   cy.get('#loginFrm_password').type(user.password)
   cy.get('button[title="Login"]').click();
   cy.get('h1.heading1').should('contain', user.firstName).and('contain', "My Account")
 
-
-  cy.getCookies('AC_SF_8CEFDA09D5').should('exist');
-
-
+  cy.getCookie('AC_SF_8CEFDA09D5').should('exist');
 
 
 
